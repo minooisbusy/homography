6e8d4b1638ec23992af765f14de3c5bf63fc8f8e
minwoo <minwoo03d0@gmail.com>
2020-08-10 15:16:51 +0900

    argument fix

--- src/main.cc
+++ src/main.cc
@@ -8,7 +8,10 @@ int main(int argc, char* argv[])
 {
     std::srand(time(0));
     DATASET::Dataset ds(argv[1],argv[2]); // load data
-    FRAME::FramePair fp(ds.next(),ds.next());
+    std::string arg1 = ds.next();
+    std::string arg2 = ds.next();
+
+    FRAME::FramePair fp(arg2,arg1);
 
     //fp.showPairs();
     fp.compute();
